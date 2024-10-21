12a438bd8da4baefc616f3a908eeafeff47ff63d
kartikmetagudd <kartikmetaguddpro@gmail.com>
2024-10-22 00:04:51 +0530

    This is new version has some color coding in it and with new interface

--- 3t.cpp
+++ 3t.cpp
@@ -1,20 +1,42 @@
 #include<iostream>
 #include<string>
 #include<unistd.h>
+#include<windows.h>
+#include<cstring>
+
+
 using namespace std;
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+
+
 void move(char& player,char& shape);
 string p1,p2;
 char ttt[3][3]=
 
-{{'_','_','_'},
-{'_','_','_'},
-{'_','_','_'}};
+{{' ',' ',' '},
+{' ',' ',' '},
+{' ',' ',' '}};
 
 
 void reset(){
      for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            ttt[i][j]='_';
+            ttt[i][j]=' ';
         }
     }
 }
@@ -37,22 +59,50 @@ bool checkWin(char player) {
 
 
 void winner() {
-    if (checkWin('X'))
-    cout << p1 << " wins..!" << endl;
+    cout<<MAGENTA;
+    if (checkWin('X')){
+        cout << p1 << " wins..!" << endl;
+        cout<<RESET;
+    }  
 
-    else if (checkWin('O'))
+    else if (checkWin('O')){
     cout << p2 << " wins..!" << endl;
+    cout<<RESET;
     }
-
+}
 
 void display(){
     for(int i=0;i<3;i++){
-        cout<<" |";
+        cout<<"\t  ";
         for(int j=0;j<3;j++){
-            cout<<" "<<ttt[i][j]<<"  ";
+            if (ttt[i][j]=='X'){
+                if(j==2){
+                    cout<<RED<<ttt[i][j]<<"     "<<RESET;
+                }
+                else 
+                cout<<RED<<ttt[i][j]<<RESET<<CYAN<<"  |  "<<RESET;
+            }
+            else if(ttt[i][j]=='O'){
+                if(j==2){
+                    cout<<YELLOW<<ttt[i][j]<<RESET<<CYAN<<"    "<<RESET;
+                }
+                else 
+                cout<<YELLOW<<ttt[i][j]<<RESET<<CYAN<<"  |  "<<RESET;
+
+            }
+            else if(j==2) 
+            cout<<RESET<<ttt[i][j]<<CYAN<<"     "<<RESET;
+            else 
+            cout<<RESET<<ttt[i][j]<<CYAN<<"  |  "<<RESET;
+
         }
-        cout<<"|"<<endl<<endl;
+        if(i<2)
+        cout<<endl<<CYAN<<"\t ----|-----|----"<<RESET<<endl;
+        else 
+        cout<<endl;
+        
     }
+    
 }
 
 
@@ -61,20 +111,23 @@ void input(int& turn) {
     bool validInput = false;
 
     while (!validInput) {
+        cout<<MAGENTA;
 
         if (turn % 2 == 0) {
             cout << ">> "<<p2<< "'s turn, Enter coordinates with a space between them (row , column): ";
         } else {
             cout << ">> "<<p1<< "'s turn, Enter coordinates with a space between them (row , column) : ";
         }
+        cout<<RESET;
 
         cin >> a >> b;
 
         // Validate the input coordinates
-        if (a >= 0 && a <= 2 && b >= 0 && b <= 2 && ttt[a][b] == '_') {
+        if (a >= 0 && a <= 2 && b >= 0 && b <= 2 && ttt[a][b] == ' ') {
             validInput = true; // Exit the loop if input is valid
 
             if (turn % 2 == 0) {
+                
                 ttt[a][b] = 'O';
             } else {
                 ttt[a][b] = 'X';
@@ -86,7 +139,9 @@ void input(int& turn) {
             // Increment the turn only after a valid move
             turn++;
         } else {
+            cout<<MAGENTA;
             cout << " Invalid input! Try again..." << endl;
+            cout<<RESET;
         }
         
     }
@@ -94,10 +149,12 @@ void input(int& turn) {
 
 
 void engin(){
+
     string name;
     bool right=false;
     char mark;
     cin.sync();
+    cout<<MAGENTA;
     cout<<">> Your Name : ";
     getline(cin,name);
     char player ,shape;
@@ -129,21 +186,27 @@ void engin(){
         if(turn%2!=0){
             right=false;
             while(!right){
+                cout<<MAGENTA;
                 cout<<">> Enter co-ordinates with space between them (row , column) : ";
+                cout<<RESET;
                 cin>>a>>b;
                 cout<<endl;
-                if (a >= 0 && a <= 2 && b >= 0 && b <= 2 && ttt[a][b] == '_'){
+                if (a >= 0 && a <= 2 && b >= 0 && b <= 2 && ttt[a][b] == ' '){
                     ttt[a][b]=player;
                     right=true;
                     display();
                     winner();
                     turn++;
                     loop++;
-                }else 
+                }else {
+                cout<<MAGENTA;
                 cout<<" Invalid input :( "<<endl<<endl;
+                cout<<RESET;
                 }
             }
+        }
         else {
+            cout<<GREEN;
             cout<<" (@ _ @)";
             sleep(1);
             cout<<".";
@@ -159,9 +222,14 @@ void engin(){
         }
         if(checkWin('X')||checkWin('O'))
         break;
-        else if(loop>=9)
+
+        else if(loop>=9){
+        cout<<MAGENTA;
         cout<<" Its a draw "<<endl;
+        cout<<RESET;
         }
+
+    }
 }
 
 
@@ -169,13 +237,13 @@ void move(char& player,char& shape){
     bool val,val2,MoveMade=false;
     for(int i=0;i<3&&!MoveMade;i++){
         for(int j=0;j<3&&!MoveMade;j++){
-            if(ttt[i][j]=='_'){
+            if(ttt[i][j]==' '){
                 ttt[i][j]=player;
                 val=checkWin(player);
-                ttt[i][j]='_';
+                ttt[i][j]=' ';
                 ttt[i][j]=shape;
                 val2=checkWin(shape);
-                ttt[i][j]='_';
+                ttt[i][j]=' ';
                 if(val||val2){
                     ttt[i][j]=shape;
                     MoveMade=true;
@@ -186,7 +254,7 @@ void move(char& player,char& shape){
         if(!MoveMade){
             for(int i=0;i<3&&!MoveMade;i++){
                 for(int j=0;j<3&&!MoveMade;j++){
-                    if(ttt[i][j]=='_'){
+                    if(ttt[i][j]==' '){
                         ttt[i][j]=shape;
                         MoveMade=true;
 
@@ -194,9 +262,6 @@ void move(char& player,char& shape){
             }
         }
         }
-
-    
- 
 }
 
 
@@ -204,16 +269,21 @@ void move(char& player,char& shape){
 
 
 void start(){
+
     system("cls");
     cout<<endl;
     bool right=false;
     int turn =1;
     char choice;
+    cout<<GREEN;
     cout<<"______TIC-TAC-TOE______"<<endl<<endl;
     cout<<"1] ENGIN(Computer) "<<endl;
-    cout<<"2] TWO PLAYER "<<endl;
+    cout<<"2] TWO PLAYER "<<endl<<endl;
+    cout<<RESET;
     while(!right){
-        cout<<">> Enter your choice(integer 1 , 2 or 3) : ";
+        cout<<BLUE;
+        cout<<">> Enter your choice(integer 1 , 2 ) : ";
+        cout<<RESET;
         cin>>choice;
         if(choice =='1'){
             right = true;
@@ -222,10 +292,12 @@ void start(){
         else if (choice =='2'){
             right=true;
             cin.sync();
+            cout<<MAGENTA;
             cout<<">> Enter player one name : ";
             getline(cin,p1);
             cout<<">> Enter player two name : ";
             getline(cin,p2);
+            cout<<RESET;
             while(turn<10){
                 input(turn);
                 if(checkWin('X')||checkWin('O'))
@@ -233,11 +305,16 @@ void start(){
             }
         }
         else{
-        cout<<" Invalid input :( "<<endl;
+            cout<<MAGENTA;
+            cout<<" Invalid input :( "<<endl;
+            cout<<RESET;
         } 
     }
-    if(turn >9)
-    cout<<" It's a draw !!"<<endl; 
+    if(turn >9){
+        cout<<MAGENTA;
+        cout<<" It's a draw !!"<<endl; 
+        cout<<RESET;
+    }    
     
 }
 
@@ -247,7 +324,9 @@ int main(){
     while(1){
         start();
         reset();
+        cout<<MAGENTA;
         cout <<">> Enter 0 to 'Quit' or Enter any key(alphabet) to 'PlayAgain' : ";
+        cout<<RESET;
         cin>>again;
         if(again=='0')
         exit(0);
---
+++ README.txt
@@ -0,0 +1,31 @@
+# Tic-Tac-Toe Game
+
+This is a simple Tic-Tac-Toe game implemented in C++. It allows for single-player and two-player modes.
+
+## Requirements
+
+- A C++ compiler (e.g., MinGW, g++) installed on your system.
+
+## Getting Started
+
+### 1. Clone the Repository
+
+If you haven't already, clone this repository to your local machine:
+
+```bash
+git clone https://github.com/kartik-metagudd/TIC-TAC-TOE.git
+cd tic-tac-toe
+
+### 2. Running the Game
+
+After cloning the repository, you can run the game using the executable provided.
+
+#### For Windows
+
+Navigate to the directory where the `3t.exe` file is located and run:
+
+command line (cmd)
+run the command - 3t.exe
+
+for linux 
+run the command - ./3t
